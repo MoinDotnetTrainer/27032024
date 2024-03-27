d971bc1476db2ae300209a1f2ed47aabbf7fc720
moin <onltonemoin@gmail.com>
2024-03-27 14:46:42 +0530

    Add project files.

---
+++ ConsoleAppGit27032024/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGit27032024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Propexample obj = new Propexample();
+
+                obj.x_ = 55;
+                obj.y_ = 49;
+
+                obj.add();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+         
+        }
+    }
+}
---
+++ ConsoleAppGit27032024/Propexample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGit27032024
+{
+    internal class Propexample
+    {
+        private int x;
+
+        public int x_
+        {
+            get { return x; }
+            set
+            {
+                if (value > 50 )
+                {
+                    throw new ArgumentException( "value cannit be greater than 50" ); 
+                }
+                else
+                {
+                    x = value;
+                }
+            }
+        }
+
+        private int y;
+
+        public int y_
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public void add()
+        {
+            int z = x + y;
+            Console.WriteLine("Add is:" + z);
+        }
+
+
+    }
+}
